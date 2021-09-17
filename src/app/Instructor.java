c32c3cd85d6209ced6e5224af6a9e6650a82a124
Hasith Yaggahavita <hasith@gmail.com>
2021-09-17 10:52:29 +0530

    task 4 test solutions

--- src/app/Instructor.java
+++ src/app/Instructor.java
@@ -2,29 +2,9 @@ package app;
 
 
 public class Instructor extends Person implements IPerson  {
-    
-    private String name;
-
-    private String nic;
 
     public Instructor() {
 
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNIC() {
-        return this.nic;
-    }
-
-    public void setNIC(String nic) {
-        this.nic = nic;
     }
 
 }
--- src/app/Student.java
+++ src/app/Student.java
@@ -3,8 +3,6 @@ package app;
 
 public class Student extends Person implements IPerson{
 
-    private String nic;
-
     public Student() {
         
     }
@@ -18,17 +16,6 @@ public class Student extends Person implements IPerson{
         this.nic = nic;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNIC() {
-        return this.nic;
-    }
-
-    public void setNIC(String nic) {
-        this.nic = nic;
-    }
 
     public String toString() {
         StringBuilder sb = new StringBuilder("Name: " + this.name);
