b91a37e25196e008228f4f95944690122a681d58
Hasith Yaggahavita <hasith@gmail.com>
2021-09-17 12:28:55 +0530

    task 5 teast

--- src/tests/Task_5_implement_getAge.java
+++ src/tests/Task_5_implement_getAge.java
@@ -1,29 +1,23 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import org.junit.Test;
 
 import app.Student;
 import java.lang.reflect.Method;
 
 public class Task_5_implement_getAge {
-
     @Test
-    public void is_setBirthYear_method_exists_in_student() {
-        try {
-            var year = 2000;
-            Student student = new Student();
+    public void is_setBirthYear_method_exists_in_student() throws Exception {
+        var year = 2000;
+        Student student = new Student();
 
-            Method set = Student.class.getMethod("setBirthYear", int.class);
-            set.invoke(student, year);
+        Method set = Student.class.getMethod("setBirthYear", int.class);
+        set.invoke(student, year);
 
-            Method get = Student.class.getMethod("getBirthYear");
-            var yearResult = get.invoke(student, year);
+        Method get = Student.class.getMethod("getBirthYear");
+        var yearResult = get.invoke(student);
 
-            assertEquals(year, ((Integer)yearResult).intValue());
-        } catch (Exception e) {
-            fail("Student class should inherit setBirthYear, getBirthYear methods from Person abstract class");
-        }
+        assertEquals(year, ((Integer) yearResult).intValue());
     }
 }
