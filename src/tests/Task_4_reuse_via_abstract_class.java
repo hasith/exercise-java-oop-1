499d39e34b237468920530e3b048d85452308f50
Hasith Yaggahavita <hasith@gmail.com>
2021-09-17 10:50:38 +0530

    task 4 test

--- src/tests/Task_4_reuse_via_abstract_class.java
+++ src/tests/Task_4_reuse_via_abstract_class.java
@@ -3,6 +3,8 @@ package tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
+
+import app.Instructor;
 import app.Person;
 import app.Student;
 import java.lang.reflect.Method;
@@ -55,4 +57,73 @@ public class Task_4_reuse_via_abstract_class {
         }
     }
 
+    @Test
+    public void is_student_setName_inherited_from_person() {
+        try {
+            Method m = Student.class.getMethod("setName", String.class);
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Student class should inherit setName method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_student_getNIC_inherited_from_person() {
+        try {
+            Method m = Student.class.getMethod("getNIC");
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Student class should inherit getNIC method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_student_setNIC_inherited_from_person() {
+        try {
+            Method m = Student.class.getMethod("setNIC", String.class);
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Student class should inherit setNIC method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_instructor_getName_inherited_from_person() {
+        try {
+            Method m = Instructor.class.getMethod("getName");
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Instructor class should inherit getName method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_instructor_setName_inherited_from_person() {
+        try {
+            Method m = Instructor.class.getMethod("setName", String.class);
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Instructor class should inherit setName method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_instructor_getNIC_inherited_from_person() {
+        try {
+            Method m = Instructor.class.getMethod("getNIC");
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Instructor class should inherit getNIC method from Person abstract class");
+        }
+    }
+
+    @Test
+    public void is_instructor_setNIC_inherited_from_person() {
+        try {
+            Method m = Instructor.class.getMethod("setNIC", String.class);
+            assertEquals(Person.class, m.getDeclaringClass());
+        } catch (NoSuchMethodException e) {
+            fail("Instructor class should inherit setNIC method from Person abstract class");
+        }
+    }
 }
