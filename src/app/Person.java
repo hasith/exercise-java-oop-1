b4f9b2c925e5ea78718815f48f3f481a14d3051d
Hasith Yaggahavita <hasith@gmail.com>
2021-09-17 10:21:49 +0530

    task 4 solutions

--- src/app/Person.java
+++ src/app/Person.java
@@ -1,5 +1,23 @@
 package app;
 
 public abstract class Person {
+
+    private String name;
+    private String nic;
     
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNIC() {
+        return this.nic;
+    }
+
+    public void setNIC(String nic) {
+        this.nic = nic;
+    }
 }
